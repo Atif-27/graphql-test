c069ce93cd154359a24acec025ed28697ce55954
Atif Ali <atif276ali@gmail.com>
2024-10-10 21:27:17 +0530

    Added graphql backend

---
+++ backend/dist/index.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import axios from 'axios';
+async function startServer() {
+    const app = express();
+    const server = new ApolloServer({
+        typeDefs: `
+            type User{
+                id:ID!
+                name:String!
+                email:String!
+                website:String!
+            }
+            type Todo{
+                userId:ID!
+                id:ID!
+                title:String!
+                completed:Boolean!
+                user:User   
+            }
+            type Query{
+                getTodos:[Todo]
+                getAllUsers:[User]
+                getUserById(id:ID!): User
+            }
+        `,
+        resolvers: {
+            Todo: {
+                user: async (parent) => (await axios.get(`https://jsonplaceholder.typicode.com/users/${parent?.userId}`)).data
+            },
+            Query: {
+                getTodos: async () => {
+                    const data = await axios.get('https://jsonplaceholder.typicode.com/todos');
+                    return data.data || [];
+                },
+                getAllUsers: async () => {
+                    const data = await axios.get('https://jsonplaceholder.typicode.com/users');
+                    return data.data || [];
+                },
+                getUserById: async (parent, { id }) => {
+                    const data = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+                    return data.data || null;
+                }
+            }
+        },
+    });
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    await server.start();
+    app.use('/graphql', expressMiddleware(server));
+    app.listen(4000, () => console.log('Server is running on port 4000'));
+}
+startServer();
